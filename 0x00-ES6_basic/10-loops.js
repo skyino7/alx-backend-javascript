a84acb8e71c4b8e0f893b0ff3401ccdfb63f70f0
Seth Ketsi <christambassador12@gmail.com>
2024-04-09 00:12:52 +0000

    Eslint fixed

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   let i = 0;
-  const arr = array
+  const arr = array;
   for (const value of array) {
     arr[i] = appendString + value;
     i += 1;
