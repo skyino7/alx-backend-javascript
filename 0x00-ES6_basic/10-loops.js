c7afe0c83d208629a47f0ba2900507cca2efd2bb
Seth Ketsi <christambassador12@gmail.com>
2024-04-09 00:11:25 +0000

    updated

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,3 +1,10 @@
 export default function appendToEachArrayValue(array, appendString) {
-  return array.map((value) => appendString + value);
+  let i = 0;
+  const arr = array
+  for (const value of array) {
+    arr[i] = appendString + value;
+    i += 1;
+  }
+
+  return arr;
 }
