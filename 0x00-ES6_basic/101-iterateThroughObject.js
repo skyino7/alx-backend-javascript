e5d863db9669a37b275c452ca07894c73ace4186
Seth Ketsi <christambassador12@gmail.com>
2024-04-09 00:52:02 +0000

    Updated

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,7 +1,7 @@
 export default function iterateThroughObject(reportWithIterator) {
-    let employees = [];
-    for (const employee of reportWithIterator) {
-        employees.push(employee);
-    }
-    return employees.join(' | ');
+  const employees = [];
+  for (const employee of reportWithIterator) {
+    employees.push(employee);
+  }
+  return employees.join(' | ');
 }
