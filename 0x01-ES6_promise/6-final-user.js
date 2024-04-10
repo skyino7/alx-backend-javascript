e080fbe70507989a0cca13f5e810fbed1a2dc2eb
Seth Ketsi <christambassador12@gmail.com>
2024-04-10 22:05:36 +0000

    6. Handle multiple promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,10 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, photo) {
+  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(photo)])
+    .then((results) => results.map(({ status, value, reason }) => ({
+      status,
+      value: status === 'fulfilled' ? value : reason.toString(),
+    })));
+}
