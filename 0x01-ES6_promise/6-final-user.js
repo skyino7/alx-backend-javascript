de43b9421acd7d5929d97c32a1a6237ad1497a49
Seth Ketsi <christambassador12@gmail.com>
2024-04-10 22:23:47 +0000

    Updated

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,10 +1,10 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, photo) {
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(photo)])
-    .then((results) => results.map(({ status, value, reason }) => ({
-      status,
-      value: status === 'fulfilled' ? value : reason.toString(),
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
     })));
 }
