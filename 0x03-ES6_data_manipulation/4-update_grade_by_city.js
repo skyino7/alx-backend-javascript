d242bbedf0ca47518f13d59a1986b9eecda4b3b6
Seth Ketsi <christambassador12@gmail.com>
2024-04-15 20:45:51 +0000

    Updated

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,9 +2,9 @@ function updateStudentGradeByCity(students, city, newGrades) {
   return students
     .filter((student) => student.location === city)
     .map((student) => {
-      const grade = newGrades.filter((newGrade) => newGrade.studentId === student.id);
-      student.grade = grade.length ? grade[0].grade : 'N/A';
-      return student;
+      const matchingGrades = newGrades.filter((newGrade) => newGrade.studentId === student.id);
+      const grade = matchingGrades.length ? matchingGrades[0].grade : 'N/A';
+      return { ...student, grade };
     });
 }
 
