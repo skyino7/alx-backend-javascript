62bb660670c79ed8135a7db6a5c71b5b3bc3c134
Seth Ketsi <christambassador12@gmail.com>
2024-04-15 20:28:14 +0000

    3. Reduce

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,6 @@
+function getStudentIdsSum(students) {
+  if (!Array.isArray(students)) return 0;
+  return students.reduce((sum, student) => sum + student.id, 0);
+}
+
+export default getStudentIdsSum;
