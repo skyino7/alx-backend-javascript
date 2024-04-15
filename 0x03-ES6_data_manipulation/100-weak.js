968044795d100949402a41e178e513bc1941b782
Seth Ketsi <christambassador12@gmail.com>
2024-04-15 22:12:26 +0000

    updated

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,17 +1,16 @@
 const weakMap = new WeakMap();
 
 function queryAPI(endpoint) {
-    if (!weakMap.has(endpoint)) {
-        weakMap.set(endpoint, 0);
-    }
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
 
-    const count = weakMap.get(endpoint) + 1;
+  const count = weakMap.get(endpoint) + 1;
 
-    if (count >= 5) {
-        throw new Error('Endpoint load is high');
-    }
-    weakMap.set(endpoint, count);
-
+  if (count >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+  weakMap.set(endpoint, count);
 }
 
 export { weakMap, queryAPI };
