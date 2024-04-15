8771bc2a26622c11859ff510fdf045037089b953
Seth Ketsi <christambassador12@gmail.com>
2024-04-15 22:06:18 +0000

    11. Weak link data structure

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,13 @@
+const weakMap = new WeakMap();
+
+function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) weakMap.set(endpoint, 0);
+
+  const count = weakMap.get(endpoint);
+
+  weakMap.set(endpoint, count + 1);
+
+  if (count >= 5) throw new Error('Endpoint load is high');
+}
+
+export { weakMap, queryAPI };
