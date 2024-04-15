ea10885fde890e01eae13002e3335266613ba403
Seth Ketsi <christambassador12@gmail.com>
2024-04-15 21:44:09 +0000

    9. Map data structure

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,12 @@
+function groceriesList() {
+  const groceries = new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 10],
+    ['Rice', 10],
+    ['Banana', 5],
+  ]);
+  return groceries;
+}
+
+export default groceriesList;
