f3cfc3068a3b41640de6536f81fb80b76616bce3
Seth Ketsi <christambassador12@gmail.com>
2024-06-03 22:21:04 +0000

    0. Executing basic javascript with Node JS

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,12 @@
+/**
+ * Display the message in the console with STDOUT
+ *
+ * @param {string} message - The message to be Displayed in the console.
+ * @return {String} This function returns a String Value
+ */
+
+const displayMessage = (message) => {
+    console.log(message);
+};
+
+module.exports = displayMessage;
