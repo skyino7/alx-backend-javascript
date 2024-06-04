bfe4685115f008aab12181f9480a280d441829c6
Seth Ketsi <christambassador12@gmail.com>
2024-06-04 19:10:24 +0000

    3. Reading a file asynchronously with Node JS

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,48 @@
+/**
+ * Counts the students in a CSV data file asynchronously.
+ * @param {String} path The path to the CSV data file.
+ * @returns {Promise} A promise that resolves when the file is read and processed.
+*/
+
+const fs = require('fs');
+
+const countStudents = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
+
+    const lines = data.trim().split('\n').filter((line) => line.trim() !== '');
+    if (lines.length <= 1) {
+      resolve();
+      return;
+    }
+
+    const studentGroups = {};
+    const dbField = lines[0].split(',');
+    const studentNames = dbField.slice(0, dbField.length - 1);
+
+    for (const line of lines.slice(1)) {
+      const studentRecord = line.split(',');
+      const studentValues = studentRecord.slice(0, studentRecord.length - 1);
+      const field = studentRecord[studentRecord.length - 1];
+      if (!Object.keys(studentGroups).includes(field)) {
+        studentGroups[field] = [];
+      }
+      const studentEntries = studentNames.map((propName, idx) => [propName, studentValues[idx]]);
+      studentGroups[field].push(Object.fromEntries(studentEntries));
+    }
+
+    const totalStudents = Object.values(studentGroups).reduce((sum, group) => sum + group.length, 0);
+    console.log(`Number of students: ${totalStudents}`);
+    for (const [field, group] of Object.entries(studentGroups)) {
+      const studentNames = group.map((student) => student.firstname).join(', ');
+      console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
+    }
+
+    resolve();
+  });
+});
+
+module.exports = countStudents;
