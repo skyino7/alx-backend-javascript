e1262c37f6cc97a92d40403025d5d5c47312eb3c
Seth Ketsi <christambassador12@gmail.com>
2024-06-04 22:10:53 +0000

    5. Create a more complex HTTP server using Node's HTTP module

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,83 @@
+const http = require('http');
+const url = require('url');
+
+/**
+ * Counts the students in a CSV data file asynchronously.
+ * @param {String} path The path to the CSV data file.
+ * @returns {Promise} A promise that resolves when the file is read and processed.
+*/
+
+const fs = require('fs');
+
+const countStudents = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
+
+    const lines = data.trim().split('\n').filter((line) => line.trim() !== '');
+    if (lines.length <= 1) {
+      resolve();
+      return;
+    }
+
+    const studentGroup = {};
+    const dbField = lines[0].split(',');
+    const studentNames = dbField.slice(0, dbField.length - 1);
+
+    for (const line of lines.slice(1)) {
+      const studentRecord = line.split(',');
+      const studentValues = studentRecord.slice(0, studentRecord.length - 1);
+      const field = studentRecord[studentRecord.length - 1];
+      if (!Object.keys(studentGroup).includes(field)) {
+        studentGroup[field] = [];
+      }
+      const studentEntries = studentNames.map((propName, idx) => [propName, studentValues[idx]]);
+      studentGroup[field].push(Object.fromEntries(studentEntries));
+    }
+
+    const totalStudents = Object.values(studentGroup).reduce((sum, group) => sum + group.length, 0);
+    console.log(`Number of students: ${totalStudents}`);
+    for (const [field, group] of Object.entries(studentGroup)) {
+      const studentNames = group.map((student) => student.firstname).join(', ');
+      console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
+    }
+
+    resolve();
+  });
+});
+
+const dbPath = process.argv[2];
+
+const server = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+  const path = parsedUrl.pathname;
+
+  if (path === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello Holberton School!');
+  } else if (path === '/students') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    countStudents(dbPath)
+      .then((data) => {
+        res.end(`This is the list of our students\n${data}.`);
+      })
+      .catch((err) => {
+        res.statusCode = 500;
+        res.end(`This is the list of our students\n${err.message}`);
+      });
+  } else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Not Found');
+  }
+});
+
+server.listen(1245, () => {
+  console.log('Server listening on port 1245');
+});
+
+module.exports = server;
