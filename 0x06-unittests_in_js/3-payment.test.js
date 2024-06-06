9829c3bf59f895fe97bcb2b163b7aa8dea7f9f4d
Seth Ketsi <christambassador12@gmail.com>
2024-06-06 22:53:38 +0000

    Added a new test

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -2,11 +2,17 @@ const sinon = require('sinon');
 const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./3-payment');
 
-describe('sendPaymentRequestToApi', function() {
-    it('should call calculateNumber with SUM', function() {
-        const spy = sinon.spy(Utils, 'calculateNumber');
-        sendPaymentRequestToApi(100, 20);
-        sinon.assert.calledWith(spy, 'SUM', 100, 20);
-        spy.restore();
-    });
+describe('sendPaymentRequestToApi', () => {
+  it('should call Utils.calculateNumber', () => {
+    const spy = sinon.spy(Utils, 'calculateNumber');
+    sendPaymentRequestToApi(100, 20);
+    sinon.assert.calledWith(spy, 'SUM', 100, 20);
+    spy.restore();
+  });
+  it('should call Utils.calculateNumber with SUM, 10, and 5', () => {
+    const spy = sinon.spy(Utils, 'calculateNumber');
+    sendPaymentRequestToApi(10, 5);
+    sinon.assert.calledWith(spy, 'SUM', 10, 5);
+    spy.restore();
+  });
 });
